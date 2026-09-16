916eebccc00a5389a14cc12b6817daffa7594e60
ghandi-hub <sonnyaghandi@gmail.com>
2026-09-16 21:07:57 +0700

    vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,3 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default { preprocess: vitePreprocess(), kit: { adapter: adapter() } };
